90bd57e867a3c195dc60c005bd0038a2ee1966fa
Deni <00amfibinator00@mail.ru>
2024-03-17 21:37:43 +0300

    последние штрихи

--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,6 @@
 import * as React from 'react';
 import { createRoot } from 'react-dom/client';
-import {
-  AdaptivityProvider,
-  ConfigProvider,
-} from '@vkontakte/vkui';
+import { AdaptivityProvider, ConfigProvider } from '@vkontakte/vkui';
 import '@vkontakte/vkui/dist/vkui.css';
 import App from './App';
 import { Provider } from 'react-redux';
